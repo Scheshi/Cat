8c4652676fcfee5b5d3fd71a344be2e27d861183
Darari <enjerudar@gmail.com>
2021-03-31 22:33:28 +0700

    foundation logic

---
+++ Assets/Scripts/Datas/StoryQuests.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq;
+
+namespace Datas
+{
+    public class StoryQuests
+    {
+        public Quest[] Quests;
+        public int currentId;
+
+        public void StartStory()
+        {
+            currentId = 0;
+        }
+        
+        public void NextQuest()
+        {
+            Quests[currentId].isComplete = true;
+            currentId++;
+        }
+    }
+}
